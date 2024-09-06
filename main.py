4eea4347447903b2a24863695494fb49cc0fccb2
0nur0duncu <oduncu.onur4806@gmail.com>
2024-09-06 13:06:45 +0300

    part-2 yüklendi.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -14,5 +15,27 @@ async def post():
 @app.put("/")
 async def put():
     return {"message": "Hello from put route"}
+
+@app.get("/items")
+async def get_items():  
+    return {"message": "Hello from get items route"}
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: int):
+    return {"message": "Hello from get item route", "item_id": item_id}
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    diary = "diary"
+
+@app.get("/food/{food_type}")
+async def get_food(food_type: FoodEnum):
+    if food_type == FoodEnum.fruits:
+        return {"message": "Hello from get fruits route"}
+    elif food_type == FoodEnum.vegetables:
+        return {"message": "Hello from get vegetables route"}
+    elif food_type == FoodEnum.diary:
+        return {"message": "Hello from get diary route"}
 
 # uvicorn main:app --host 127.0.0.1 --port 5000 --reload
