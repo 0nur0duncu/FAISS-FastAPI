78058bc2ff623c2b1b3301dbd6867314230a7de4
0nur0duncu <oduncu.onur4806@gmail.com>
2024-09-06 15:26:25 +0300

    part-4 yüklendi.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from enum import Enum
 from typing import Optional, Union
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -46,5 +47,38 @@ async def get_food(food_type: FoodEnum):
         return {"message": "Hello from get vegetables route"}
     elif food_type == FoodEnum.diary:
         return {"message": "Hello from get diary route"}
+    
+
+class Item(BaseModel): # request body
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+@app.post("/items")
+async def create_item(item : Item): #-> Item
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def create_item_with_put(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
 
 # uvicorn main:app --host 127.0.0.1 --port 5000 --reload
+"""
+raw body:
+{
+    "name": "Hi",
+    "description": "Hello World", varsayılan değer None olduğu için ybody de yazılmayabilir.
+    "price": 0,
+    "tax": 0
+}
+header:
+"Content-Type":application/json
+"""
