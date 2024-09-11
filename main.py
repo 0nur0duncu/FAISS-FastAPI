a5a436d1ce1a9770de4c45610092dd8e0c52b163
Onur Oduncu <160393966+0nur0duncu@users.noreply.github.com>
2024-09-11 14:38:33 +0300

    DeclareRequestExampleData yüklendi

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, Path, Body
+from fastapi import FastAPI, Query, Path, Body, Cookie, Header
 from enum import Enum
 from typing import Optional, Union, List
 from pydantic import BaseModel, Field
@@ -7,44 +7,33 @@ import re
 app = FastAPI()
 
 """
-Part 9: Body - Nested Models
+Part 10: Declare Request Example Data
 """
 
-class Image(BaseModel):
-    url: str = Field(..., regex=re("^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)"))
-    name: str
-
 class Item(BaseModel):
-    name: str
-    description: Union[str, None] = Field(None, title="Description of the item", max_length=300)
-    price : float = Field(..., gt=0, description="The price must be greater than zero")
-    tax: Union[float, None] = None
-    # tags : List[int] = []
-    tags : set[str] = set()
-    # image : Union[Image, None] = None
-    image: Union[List[Image], None] = None
-
-class Offer(BaseModel):
+    """ name: str = Field(..., example="Foo")
+    description: Union[str, None] = Field(None, example="A very nice Item")
+    price: float = Field(..., example=16.25)
+    tax: Union[float, None] = Field(None, example=10.5) """
     name: str
     description: Union[str, None] = None
     price: float
-    items: list[Item]
+    tax: Union[float, None] = []
+
+    class Config: # yöntem 1
+        json_schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 16.25,
+                "tax": 1.67
+            }
+        }
 
 @app.put("/items/{item_id}")
 async def update_item(
-    item_id: int, item: Item = Body(...,embed=True)
+    item_id: int,
+    item: Item = Body(..., examples={"normal": {"name": "Foo", "description": "A very nice Item", "value" :{"price": 16.25, "price": 1.67}}, "converted": { "summary": "An example with converted data", "description": "FastAPI can convert prices 'string'", "value": {"name":"bar", "price":"16.25"}}, "invalid": {"summary":"invalid data is rejected with an error", "value": {"name":"Baz", "price":"sixteen point twenty five"}}}),
 ):
-    result = {"item_id": item_id, "item": item}
-    return result
-
-@app.put("/offers")
-async def create_offer(offer: Offer = Body(...,embed=True)):
-    return offer
-
-@app.post("/images/multiple")
-async def create_multiple_images(images: List[Image] = Body(...,embed=True)):
-    return images
-
-@app.post("blah")
-async def create_some_item(blah: dict[int, float]):
-    return blah
+    results = {"item_id": item_id, "item": item}
+    return results
